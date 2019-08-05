648cca7eb5e1c06086a2e3d0ecc0710bb22177f6
rickkas7 <rickk@rickk.com>
2019-08-05 14:01:03 -0400

    Version 1.5.6
    - Port of Adafruit 1.5.6 verison

--- update.bash
+++ update.bash
@@ -6,6 +6,6 @@ cp -v $ADAFRUIT_REPO/Fonts/*.h src
 cp -v $ADAFRUIT_REPO/Adafruit_GFX.cpp src/Adafruit_GFX_RK.cpp 
 cp -v $ADAFRUIT_REPO/Adafruit_GFX.h src/Adafruit_GFX.h 
 
-# The SPITFT files are modified and need to be manually merged
-# cp -v $ADAFRUIT_REPO/Adafruit_SPITFT* src/
+# The SPITFT files no longer need to be modified and can be used as-is
+cp -v $ADAFRUIT_REPO/Adafruit_SPITFT* src/
  
